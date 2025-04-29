e2be1b24e3d7e3f5fe5d7ad8f89303874b28c16c
Sazuzaki <sazuzaki@gmail.com>
2025-04-29 18:12:43 -0400

    Add mana regen

--- src/main/java/sazu/adventureeffects/AdventureEffects.java
+++ src/main/java/sazu/adventureeffects/AdventureEffects.java
@@ -2,11 +2,17 @@ package sazu.adventureeffects;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sazu.adventureeffects.effect.ManaRegen;
 
 public class Adventureeffects implements ModInitializer {
 	public static final String MOD_ID = "adventureeffects";
+	public static final StatusEffect MANA_REGEN = new ManaRegen();
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -15,6 +21,7 @@ public class Adventureeffects implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "mana_regen"), MANA_REGEN);
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
--- src/main/java/sazu/adventureeffects/effect/ManaRegen.java
+++ src/main/java/sazu/adventureeffects/effect/ManaRegen.java
@@ -1,4 +1,25 @@
 package sazu.adventureeffects.effect;
 
-public class ManaRegen {
+import com.github.theredbrain.manaattributes.ManaAttributes;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class ManaRegen extends StatusEffect {
+    public ManaRegen() {
+        super(StatusEffectCategory.BENEFICIAL, 0xe9b8b3);
+
+        this.addAttributeModifier(
+                ManaAttributes.MANA_REGENERATION, // <- the custom attribute
+                "7107DE5E-7CE8-4030-940E-514C1F160890", // random UUID, needs to be unique
+                2.0D, // 2 increase
+                EntityAttributeModifier.Operation.ADDITION
+        );
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
 }
